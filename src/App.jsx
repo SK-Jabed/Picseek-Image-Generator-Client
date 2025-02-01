4b2c68bd8fb832c749b34a84b94794406fc5f042
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-01 23:58:54 +0600

    Basic Setup for Client Side Project Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-
       <h1>Picseek Image Generator</h1>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
