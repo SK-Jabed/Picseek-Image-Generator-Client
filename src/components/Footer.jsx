62890e233f4390abecc8d62e9660c9150a4ee18a
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-14 21:35:39 +0600

    Footer Component Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            Footer Here            
+            Footer is Here            
         </div>
     );
 };
