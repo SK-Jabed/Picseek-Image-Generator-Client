cb37e00f6d4eb2c12eb540dc3a1052c7dcce3e2a
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-04 23:50:08 +0600

    Basic Routes Setup at App.jsx File Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
+import AppLayout from "./Layout/AppLayout";
+import Home from "./Pages/Home";
+import Create from "./Pages/Create";
 
 function App() {
   return (
     <>
-      <h1>Picseek Image Generator Project</h1>
+      <Routes>
+        <Route path="/" element={<AppLayout></AppLayout>}>
+          <Route index element={<Home></Home>}></Route>
+          <Route path="/create" element={<Create></Create>}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
