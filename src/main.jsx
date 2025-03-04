7c0c0915082430e9eae67ddea54d8c27baea6bdd
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-04 23:44:37 +0600

    React Router and Basic Setup Done for Client Side

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import AuthProvider from "./provider/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <App></App>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
