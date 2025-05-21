5d9e25d8d5b9a0a5019d8bd1243ee5c9c6cf1d4b
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-21 23:43:34 +0600

    [UPDATED & DONE]: Navbar, Create Image Page and Banner Section's design updated and all activities completed

--- src/pages/Creations.jsx
+++ src/pages/Creations.jsx
@@ -14,6 +14,7 @@ const Creations = () => {
   return (
     <div>
       <PageTitle>All Creations</PageTitle>
+      
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {images.map((img) => (
           <div key={img._id}>
